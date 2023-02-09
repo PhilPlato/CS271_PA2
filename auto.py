f1d20275dd4961447d081c97f5fca78f2af7449d
AndyCheng2 <luqingcheng2@gmail.com>
2023-02-08 23:04:21 -0800

    auto script

---
+++ auto.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+time.sleep(1)
+os.system('start cmd /K "python client.py A"')
+time.sleep(1)
+os.system('start cmd /K "python client.py B"')
+time.sleep(1)
+os.system('start cmd /K "python client.py C"')
+time.sleep(1)
+os.system('start cmd /K "python client.py D"')
+time.sleep(1)
+os.system('start cmd /K "python client.py E"')
